45a2cae5ebbb326dd6e9d9f67a7be67a495c4a54
unknown <treichert@WSSH004_07212.fsbwit.intern>
2015-03-18 14:43:53 +0100

    DB-Skript angepasst

--- php/createDB.sql
+++ php/createDB.sql
@@ -1,12 +1,12 @@
-drop database if exists `db_isaac`;
+drop database if exists `hs01-2`;
 
-create database if not exists db_isaac;
-alter database db_isaac default character set latin1 default collate latin1_german2_ci;
-use db_isaac;
+create database if not exists `hs01-2`;
+alter database `hs01-2` default character set latin1 default collate latin1_german2_ci;
+use `hs01-2`;
 
 drop table if exists `t_highscore`;
 
-create table t_highscore
+create table `t_highscore`
 (	id MEDIUMINT NOT NULL AUTO_INCREMENT,
         name CHAR(30) NOT NULL,
         punkte DECIMAL(65),
